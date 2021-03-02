1c11b632caaa9efa3a0950ded5715c5132545799
anisha <anisha256>
2021-03-02 17:36:06 +0545

    modified

--- minor/app.js
+++ minor/app.js
@@ -1,7 +1,6 @@
 // Imports
 const express = require('express')
 const app = express();
-
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const blockchainController = require('./controllers/blockchainController');
@@ -21,13 +20,13 @@ var corsOptions = {
 
 // require("./db/conn");
 var User = require('./models/User');
-// var Login = require('./login')
+
 
 // Static Files
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//console.log(process.env);
+
 
 // set views
 app.set('views', './views')
@@ -35,11 +34,14 @@ app.set('view engine', 'ejs')
 
 //Db connection start
 mongoose.Promise = global.Promise;
+
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
+
+
 mongoose.connection.on('error', (err) => {
-	console.error('Mongo failed to connect');
+console.error('Mongo failed to connect');
 });
 enrollAdmin();
 
@@ -47,21 +49,13 @@ app.post('/signup',
   [
     verifySignUp.checkDuplicateUsernameOrEmail
   ],
-  // logincontroller.signup
-  //blockchainController.enrollAdmin,
-  blockchainController.registerAndEnrollUser,
+ blockchainController.registerAndEnrollUser,
   responseController.ca,
 );
 
-app.post("/signin", logincontroller.signin);
-
-
-
-
-// app.post("/signin",(req,res)=>{
-//     res.render('home')
-// })
-// };
+app.post("/signin", 
+logincontroller.signin
+);
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -105,8 +99,7 @@ app.get('/quote', (req, res) => {
   console.log(token)
   res.render('quote', {
     data: {},
-    // header: new Headers({
-    //   'user': token
+
   })
 
 })
@@ -115,66 +108,33 @@ app.get('/doctor_entry', (req, res) => {
     data: {}
   })
 })
-
-
-// app.post('/doctor_entry', function (req, res) {
-//   res.render('form', {
-//     data: req.body
-//   })
-//   console.log(req.body.doctorName)
-//   var user = new Doctor({
-//     doctorName: req.body.doctorName,
-//     NMCNumber: req.body.NMCNumber,
-//     //hospitalName: res.body.hospitalName,
-//     qualification: req.body.qualification,
-//     speciality: req.body.speciality,
-
-//   })
-//   var promise = doctor.save()
-//   promise.then((doctor) => {
-//     console.log("user saved", doctor)
-//   })
-
-// })
-// app.get('/token', function(req, res){
-//   var token = jwt.sign({id: login.id}, config.secret ,{expiresIn: 120});
-//   res.send(token)
-// })
-// app.post('/quote', function(req, res){
-//     res.render('quote',{
-//         data:req.body
-//     })
-//     console.log(req.body.doctorName)
-//     console.log(req.body.patientName)
-//     var user= new User({
-//         doctorName :req.body.doctorName,
-//         patientName : req.body.patientName,
-//         // hospitalName: res.body.hospitalName,
-//         height: req.body.height,
-//         weight: req.body.weight,
-//         description: req.body.description
-//     })
-//     var promise = user.save()
-//     promise.then((user) => {
-//         console.log("user saved",user)
-
-//     })
-
-// })
-
 app.post('/quote',
+
   blockchainController.invokeChaincode,
   responseController.user
 )
 
 app.post('/quotedoctor',
+ 
   blockchainController.invokeChaincode,
   responseController.user
 )
 
 app.post('/find',
   reportController.getReportByID,
-  blockchainController.queryChaincode
+  blockchainController.queryChaincode,
+  responseController.user
+  
+  )
+app.post('/display',
+  reportController.getReports,
+  blockchainController.queryChaincode,
+  responseController.user
+)
+app.post('/update',
+  reportController.updateReportByID,
+  blockchainController.invokeChaincode,
+  responseController.user
   
   )
 
